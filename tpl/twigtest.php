07d6314612719b0376e09f9a39bd54cb9b565f11
¨Patrick <patrick@quest-bird.com>
2013-11-22 12:13:21 +1100

    added template test loader twigtest.php

---
+++ tpl/twigtest.php
@@ -0,0 +1,20 @@
+<?php
+/* a test script for Giles to run the Twig templates in this folder */
+require_once(dirname(__FILE__) . "../lib/find_path.inc.php");
+require_once ($_SERVER["DOCUMENT_ROOT"] . LIBPATH . "/lib/addons/Twig/Autoloader.php");
+Twig_Autoloader::register();
+/* prepare Twig environment */
+$loader = new Twig_Loader_Filesystem($_SERVER["DOCUMENT_ROOT"] . LIBPATH . "/tpl"); 
+$twig = new Twig_Environment($loader, array(
+					    "auto_reload" => true,
+					    
+));
+/* define variables to be used with the template */
+/* Load the actual template here */
+
+$template = $twig->loadTemplate('base.html');
+echo $template->render(array(
+'the' => 'variables',
+'go' => 'here'
+));
+?>

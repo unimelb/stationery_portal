f60c9792b157b0add5760f0b8b3b57d1cb838459
¨Patrick <patrick@quest-bird.com>
2013-12-07 17:24:31 +1100

    index.php now calls Stationery app, which loads Twig template

--- index.php
+++ index.php
@@ -1,89 +1,35 @@
-<!DOCTYPE HTML>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <title>Chili Publish : Marketing : The University of Melbourne</title>  
-
-<!-- Authoriser and maintainer related meta data - developers, don't forget humans.txt   -->
-  <meta name="DC.Creator" content="The University of Melbourne">
-
-  <meta name="UM.Authoriser.Name" content="">
-  <meta name="UM.Authoriser.Title" content="">
-  <meta name="UM.Maintainer.Name" content="">
-  <meta name="UM.Maintainer.Department" content="">
-  <meta name="UM.Maintainer.Email" content="">
-<!-- End authoriser and maintainer meta data -->
-<!-- Static meta data   -->
-  <meta name="DC.Rights" content="http://www.unimelb.edu.au/disclaimer">
-  <meta name="DC.Publisher" content="The University of Melbourne">
-  <meta name="DC.Format" content="text/html">
-  <meta name="DC.Language" content="en">
-  <meta name="DC.Identifier" content="http://www.unimelb.edu.au/">
-<!-- End static meta data -->
-<!-- Meta data to be autofilled -->
-  <meta name="DC.Date" content="">
-  <!--#config timefmt="%Y-%m-%d" --><meta name="DC.Date.Modified" content="<!--#echo var='LAST_MODIFIED' -->">
-<!-- End meta data to be autofilled -->
-  <meta content="width=device-width; initial-scale=0.67;" name="viewport">
-
-  <!-- GLOBAL RESOURCES -->
-  <!-- DO NOT CHANGE -->
-    <link rel="stylesheet" href="http://brand.unimelb.edu.au/web-templates/LATEST/css/complete.css">
-    <script src="http://brand.unimelb.edu.au/web-templates/LATEST/js/complete.js"></script>
-    <script src="http://brand.unimelb.edu.au/global-header/js/injection.js"></script>
-  <!-- /GLOBAL RESOURCES -->
-  <link rel="stylesheet" href="css/custom.css" media="screen">
-  <script type="text/javascript" src="js/custom.js"></script>
-
-
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-9675738-1']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
-</head>
-<body class="no-logo sub-page">
-	
-	<div class="wrapper">
-		
-    <div id="header" class="header">
-      
-      <div class="hgroup">
-        <h1><a href="/" title="Marketing">University Marketing</a><br />&nbsp;</h1>
-      </div>
-      <div class="nav-wrapper">
-				<?php include("includes/menu.html"); ?>
-      </div>
-    </div>
-    
-    <div class="col-6" id="main">
-			
-			<h2>University Stationery</h2>
-			<?php Echo "Hello Giles!"; ?>
-			
-		</div>
-		<div class="aside col-2">
-			<h3>Having a Problem with this Portal?</h3>
-			<p><strong>UOM Chili Publish Admin</strong><br />
-				<strong>Email:</strong> <a href="mailto:&#x63;&#x68;&#x69;&#x6C;&#x69;&#x40;&#x6C;&#x69;&#x73;&#x74;&#x73;&#x2E;&#x75;&#x6E;&#x69;&#x6D;&#x65;&#x6C;&#x62;&#x2E;&#x65;&#x64;&#x75;&#x2E;&#x61;&#x75;">chili@lists.unimelb.edu.au</a></p>
-			<hr class="visible" />
-			<p><a class="icon document large" href=""><strong>Looking to</strong>Order Stationery</a></p>
-			<hr /><br />
-		</div>
-		<hr> 
-  </div>
-  <hr>
-  <div class="footer">
-    <?php include("includes/mcd-footer.html"); ?>
-  </div>
-</body>
-</html>
+<?php
+/**
+ * The instance script to run a stationery app
+ *
+ * @author Damian Sweeney <dsweeney@unimelb.edu.au>
+ * @author Patrick Maslen <pmaslen@unimelb.edu.au>
+ */
+//session_name('stationery');
+//session_start();
+require_once(dirname(__FILE__) . "/lib/find_path.inc.php");
+//include_once($_SERVER["DOCUMENT_ROOT"] . LIBPATH . "/includes/passport.php");
+require_once($_SERVER["DOCUMENT_ROOT"] . LIBPATH . "/lib/cgiapps/stationery.class.php");
+// require_once($_SERVER["DOCUMENT_ROOT"] . LIBPATH . "/includes/dbconnect.inc.php");
+$template_path = $_SERVER["DOCUMENT_ROOT"] . LIBPATH . '/tpl/';
+// template_params must at minimum include 'filename' keyword
+$title = "University Stationery";
+//$title_attributes = "";
+$template_params = array(
+		'filename' => 'base.html',
+	  	'title' => $title,
+	  	'extra_style' => array(
+			/*array('url' => '/css/qset.css', 'media' => 'screen')*/
+			),
+	  	'extra_script' => array(
+			array(
+				)
+			)
+	);
+$params = array(
+	'template_path' => $template_path,
+	'template_params' => $template_params
+	 );
+$webapp = new Stationery(array('PARAMS' => $params));
+$webapp->run(); 
+?>

1d29dd08fea4024c40f466ea8f1b71fed098826f
Patrick Maslen <patrick@quest-bird.com>
2014-02-21 13:56:12 +1100

    fixed password to localSQL

--- setup/stationery1.sql
+++ setup/stationery1.sql
@@ -1,6 +1,6 @@
 -- setup
   CREATE DATABASE stationery;
-  INSERT INTO mysql.user (User,Host,Password) VALUES('chili_user','localhost',PASSWORD('7Fy5Z3aDhP#acgWqkXnT'));
+  INSERT INTO mysql.user (User,Host,Password) VALUES('chili_user','localhost',PASSWORD('localSQL'));
   FLUSH PRIVILEGES;
   GRANT ALL PRIVILEGES ON stationery.* to chili_user@localhost;
   FLUSH PRIVILEGES;

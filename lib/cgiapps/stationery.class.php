329fe4f630ca791b9f93fc05aed85a0ef8568881
Patrick Maslen <patrick@quest-bird.com>
2014-05-16 13:58:57 +1000

    modified select statement in $this->select to allow multiple departments

--- lib/cgiapps/stationery.class.php
+++ lib/cgiapps/stationery.class.php
@@ -203,7 +203,7 @@ class Stationery extends Cgiapp2 {
 			  'SELECT * FROM user WHERE username = :id',
 			  'SELECT name, acronym, department_id FROM department',
 			  'SELECT department_id from user_department where username = :id',
-			  "SELECT * FROM template WHERE category_id = :category_id AND department_id in ( 'jjjdepartments' ) OR category_id = :category_id2 AND department_id IS NULL ORDER BY department_id ASC",
+			  "SELECT * FROM template WHERE category_id = :category_id AND department_id in ( jjjdepartments ) OR category_id = :category_id2 AND department_id IS NULL ORDER BY department_id ASC",
 			  'SELECT * FROM job WHERE username = :username ORDER BY job_id DESC LIMIT 1',
 			  'SELECT j.job_id, j.username, c.description FROM job j, category c, template t WHERE t.template_id = j.template_id AND t.category_id = c.category_id and j.job_id = :job_id',
 			  'SELECT id FROM template WHERE template_id = :template_id AND chili_id = :chili_id',

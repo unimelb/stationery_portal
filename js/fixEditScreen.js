6f26b238ca4738ac7da5d95962462ae89a789855
Patrick Maslen <pmaslen@unimelb.edu.au@9750l-132880-l.mobility.unimelb.net.au>
2017-10-13 14:11:02 +1100

    Added fixEditScreen.js to fix vertical jumping problem

---
+++ js/fixEditScreen.js
@@ -0,0 +1,8 @@
+/* javascript tied to the tpl/edit.html screen
+ * a workaround for the jumping to the top behaviour
+ * Currently, focus is shifting to an element called input-number
+ * which has a style attribute which puts it at the top of the screen
+ * <input class="floating-element" type="number" id="input-number" style="left: -666px; top: -666px;">
+ */
+document.getElementById("input-number").style["left"] = 0;
+document.getElementById("input-number").style["top"] = 0;

32befce70e84871fa8c79afb0995747d74eca991
Patrick Maslen <pmaslen@unimelb.edu.au@9750l-132880-l.mobility.unimelb.net.au>
2017-10-13 14:25:37 +1100

    load fixEditScreen on iframe load

--- js/fixEditScreen.js
+++ js/fixEditScreen.js
@@ -4,5 +4,7 @@
  * which has a style attribute which puts it at the top of the screen
  * <input class="floating-element" type="number" id="input-number" style="left: -666px; top: -666px;">
  */
+function frameLoaded() {
 document.getElementById("input-number").style["left"] = 0;
 document.getElementById("input-number").style["top"] = 0;
+};
